0db12205a90347d13856910f16e1c5353eec1f60
aasith <aasith32@gmail.com>
2026-05-24 10:34:16 +0530

    Flask project initialized

---
+++ app.py
@@ -0,0 +1,18 @@
+from flask import Flask 
+from flask_sqlalchemy import SQLAlchemy
+
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:root123@localhost/crud'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db = SQLAlchemy(app)
+
+
+@app.route("/")
+def home():
+    return 'hello world'
+
+
+if __name__ == '__main__':
+  app.run(debug=True)
